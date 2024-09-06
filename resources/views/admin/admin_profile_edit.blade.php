4f4bd3945cf65b43c228ab041570cd385f5b3710
riyan <riyanarhm28@gmail.com>
2024-09-06 20:21:45 +0700

    store

---
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -0,0 +1,97 @@
+@extends('admin.admin_master')
+
+@section('content')
+    
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<div class="container-full">   
+		<!-- Main content -->
+		<section class="content">
+
+		 <!-- Basic Forms -->
+		  <div class="box">
+			<div class="box-header with-border">
+			  <h3 class="box-title">Edit Profile </h3> 
+			</div>
+			<!-- /.box-header -->
+			<div class="box-body">
+			  <div class="row">
+				<div class="col">
+
+					<form method="post" action="{{route('admin.profile.update')}}" enctype="multipart/form-data">
+					@csrf
+
+					  <div class="row">
+						<div class="col-12">	
+							
+							<div class="row">
+								<div class="col-6">
+									<div class="form-group">
+										<h5>Username <span class="text-danger">*</span></h5>
+										<div class="controls">
+											<input type="text" name="name" class="form-control" required=""
+											value="{{$editData->name}}"> </div>
+									</div>
+								</div>
+
+								<div class="col-6">
+									<div class="form-group">
+										<h5>Email <span class="text-danger">*</span></h5>
+										<div class="controls">
+											<input type="email" name="email" class="form-control" required=""
+											value="{{$editData->email}}"> </div>
+									</div>
+								</div>
+							</div>
+
+
+							<div class="row">
+								<div class="col-6">
+									<div class="form-group">
+										<h5>Profile image <span class="text-danger">*</span></h5>
+										<div class="controls">
+											<input type="file" id="image" name="profile_photo_path" class="form-control" required> </div>
+									</div>
+								</div>	
+
+								<div class="col-6">
+									<img class="rounded-circle" id="showImage" src="{{(!empty($adminData->profile_photo_path))? url('upload/admin_images/'.$adminData->profile_photo_path):url('upload/no_image.jpg') }}" alt="User " style="width: 100px; height:100px;">
+								</div>	
+
+
+						   <div class="text-xs-right">
+							 <input type="submit" class="btn btn-rounded btn-primary mb-5" value="update">
+						   </div>
+					  </div>
+					</form>
+
+				</div>
+				<!-- /.col -->
+			  </div>
+			  <!-- /.row -->
+			</div>
+			<!-- /.box-body -->
+		  </div>
+		  <!-- /.box -->
+
+		</section>
+		<!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+</div>
+
+
+<script type="text/javascript">
+  $(document).ready(function() {
+	$(`#image`).change(function(e) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$(`#showImage`).attr('src',e.target.result);	
+		}
+		reader.readAsDataURL(e.target.files['0']);
+	});
+  });
+</script>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\AdminProfileController;
+
+use App\Http\Controllers\frontend\IndexController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['prefix'=> 'admin', 'middleware'=>['admin:admin']], function(){
+	Route::get('/login', [AdminController::class, 'loginForm']);
+	Route::post('/login',[AdminController::class, 'store'])->name('admin.login');
+});
+
+
+Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
+    return view('admin.index');
+})->name('admin.index');
+
+//all route admin
+Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+Route::get('/admin/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
+Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
+Route::post('/admin/profile/update', [AdminProfileController::class, 'adminProfileUpdate'])->name('admin.profile.update');
+
+Route::get('/admin/change/password', [AdminProfileController::class, 'adminChangePassword'])->name('admin.change.password');
+Route::post('/admin/password/update', [AdminProfileController::class, 'adminPasswordUpdate'])->name('admin.password.update');
+
+Route::get('/',[IndexController::class, 'index']);
+
+
+
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
